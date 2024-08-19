289a1515a0b126d04bf81e25a80d88044e40593a
Antokl <liantokha@mail.ru>
2024-08-19 23:49:59 +0300

    Add project files.

---
+++ Hello Homework!.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // ќбъ€вл€ет и инициализирует строку.
+    std::string myString = "Hello Homework!";
+
+    // ѕечатает строку.
+    std::cout << myString << std::endl;
+
+    return 0;
+}
